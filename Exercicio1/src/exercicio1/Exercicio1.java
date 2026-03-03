643e504201566c3ef2c4f185ad506748dd238577
migueloliveirahahn <hahnmiguel26@gmail.com>
2026-03-03 15:13:46 -0300

    Add files via upload

---
+++ Exercicio1/src/exercicio1/Exercicio1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio1;
+
+/**
+ *
+ * @author aluno.saolucas
+ */
+public class Exercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Produto P1 = new Produto();
+        P1.setNome("Banana");
+        P1.setPreço(15.50);
+        System.out.println("o produto: " + P1.getNome() + "\n" + "sai por " + P1.getPreço());
+    }
+    
+}
---
+++ Exercicio1/src/exercicio1/Produto.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio1;
+
+/**
+ *
+ * @author aluno.saolucas
+ */
+public class Produto {
+    private String Nome;
+    private double preço;
+    
+    public String getNome() {
+    return Nome;
+    }
+    
+    public double getPreço(){
+    return preço;
+    }
+    
+    public void setNome(String Nome){
+    this.Nome = Nome;
+    }
+    
+    public void setPreço(double preço){
+    if(preço == 15.50) {
+        this.preço = preço;
+    }
+    else {
+       System.out.println("Preço inválido"); 
+    }
+    }
+}     
+
